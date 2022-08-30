52fd980a8a9bdf3ab88a8a589df5f7e30a065d65
César Daniel Alarcón Guerrero <65317680+Cesar483@users.noreply.github.com>
2022-08-30 03:04:09 -0500

    Add files via upload

---
+++ Interfaz_prueba/Interfaz_prueba/Principal.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace Interfaz_prueba
+{
+    public partial class Principal : Form
+    {
+    //***Opciones del menú****
+        public Principal()
+        {
+            InitializeComponent();
+            this.CenterToScreen();
+            this.BackColor = System.Drawing.Color.LightGreen;    
+        }
+
+// ****IP****
+        private void Ipbutton_Click(object sender, EventArgs e)
+        {
+            Regex rge = new Regex(@"\b((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4}\b");
+            if (rge.IsMatch(IpTextbox()))
+            {
+                this.ipRegEx_Pass.Text = "Valida";
+                this.ipRegEx_Pass.BackColor = Color.Green;
+            }
+            else
+            {
+                this.ipRegEx_Pass.Text = "Invalida";
+                this.ipRegEx_Pass.BackColor = Color.Red;
+            }
+        }
+        private string IpTextbox()
+        {
+            string ipvalidated = this.ipTextBox.Text;
+            return ipvalidated;
+        }
+
+
+ //  Botón de salir
+        private void Exitbutton_Click(object sender, EventArgs e)
+        {
+            //Finalizar app
+            Application.Exit();
+        }
+
+        private void Principal_Load(object sender, EventArgs e)
+        {
+
+        }
+//Mes y día de la semana******
+        private void MyDbutton_Click(object sender, EventArgs e)
+        { 
+                Regex rge = new Regex(@"^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$");
+                if (rge.IsMatch(MyDTextbox()))
+                {
+                    this.MyDRegEx_Pass.Text = "Valida";
+                    this.MyDRegEx_Pass.BackColor = Color.Green;
+                }
+                else
+                {
+                    this.MyDRegEx_Pass.Text = "Invalida";
+                    this.MyDRegEx_Pass.BackColor = Color.Red;
+                }
+            }
+            private string MyDTextbox()
+            {
+
+                string MyDvalidated = this.MyDiTextBox.Text;
+                return MyDvalidated;
+            }
+       
+
+        }
+    }
+
